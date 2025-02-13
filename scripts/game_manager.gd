b13f3781d7ad9f046448d64e144893f33653e351
Dustin Kaschmitter <thomaskasch6@gmail.com>
2025-02-12 17:08:27 -0800

    Golden Garden Update

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,9 +1,24 @@
 extends Node
 
 var score = 0
+const TOTAL1 = 15
+const TOTAL2 = 25
 
 @onready var score_label: Label = $ScoreLabel
+@onready var score_label_2: Label = $ScoreLabel2
+@onready var killzone_2: Area2D = $Killzone2
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func add_point():
 	score += 1
 	score_label.text = "You have collected \n" + str(score) + " coins."
+	if score <= TOTAL1:
+		score_label_2.text = "Collect\n" + str(TOTAL1 - score) +" More\n" + "Coins!"
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if score == TOTAL1:
+		killzone_2.queue_free() 
+		score_label_2.text = "Visit a\n Suspicious, Snowy\n Platform"
+	if score >= TOTAL2:
+		score_label_2.text = "Thank you \nfor Restoring\n Our Gold\n Please Visit\n Our Golden Garden"
+		animation_player.play("platform")
--- scripts/player.gd
+++ scripts/player.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
+@onready var camera_2d: Camera2D = $Camera2D
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
@@ -37,3 +38,7 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	camera_2d.limit_bottom = 10000
